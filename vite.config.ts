57feece965cc037fa3ed5599fcdad4835dd87e8d
awalicki <awalicki04@gmail.com>
2026-05-01 18:19:42 +0000

    fix: dynamic base url, local dev on /

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  base: '/bike_service_website/',
-
-
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/bike_service_website/' : '/',
   build: {
     outDir: 'docs',
   },
-});
+}));
